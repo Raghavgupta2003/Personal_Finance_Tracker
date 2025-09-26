9ebdde31b90ca758cb42253026db70276a9a8cba
Raghavgupta2003 <raghav>
2025-09-26 21:46:45 +0530

    frontendUrlSetup

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173', // local dev
-    'https://personal-finance-tracker.vercel.app' // frontend production
+    'https://personal-finance-tracker-nine-beta.vercel.app' // frontend production
   ],
   credentials: true
 }));
