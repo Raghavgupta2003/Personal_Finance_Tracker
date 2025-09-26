145db5abfcbabce7dd6617eb720c670d04053ded
Raghavgupta2003 <raghav>
2025-09-26 13:49:05 +0530

    envify

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_BASE_URL = '/api' ;
 
 const api = axios.create({
-  baseURL: http://localhost:5000/api,
+  baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
